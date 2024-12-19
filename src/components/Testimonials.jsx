7f3fa939f7d51767c06887ae441996a80623f21d
Bhavesh25032003 <bhaveshvinayaraj2003@gmail.com>
2024-12-19 14:50:49 +0530

    final commit

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,7 +8,7 @@ const Testimonials = () => {
          <div className="flex flex-wrap justify-center">
             {testimonials.map((testimonial, index) =>(
                 <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
-                    <div className="bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
+                    <div className="rounded-md p-6 text-md border border-neutral-800">
                         <p>{testimonial.text}</p>
                         <div className="flex mt-8 items-start">
                             <img className="w-12 h-12 mr-6 rounded-full border border-neutral-300" src={testimonial.image} alt={testimonial.user} />
@@ -16,7 +16,7 @@ const Testimonials = () => {
                                 <h6>
                                     {testimonial.user}
                                 </h6>
-                                <span className="text-sm font-normal italic text-neutral-600">
+                                <span className="text-sm font-normal italic text-neutral-500">
                                     {testimonial.company}
                                 </span>
                             </div>
